4df0051db0f5d775fa99b17a08f6de38865fc0b1
Nacho Miranda <166954662+champagneplease@users.noreply.github.com>
2025-05-16 16:38:55 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import bcrypt
+
+app = Flask(__name__)
+app.secret_key = "1234qwer.."
+
+users = {}  # username: hashed_password
+
+
+@app.route("/")
+def index():
+    user = session.get("user")
+    return render_template("index.html", user=user)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def registros():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Hashear la contraseña
+        hashed_pw = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        # Guardar el usuario con su contraseña hasheada
+        users[username] = hashed_pw
+
+        session["user"] = username
+        print(users)
+        return redirect(url_for("index"))
+
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=2009)
